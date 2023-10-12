3eaf638d79c38f780036da47ec078dbc7571a0e3
Brandon <143155030+Good-Day-2-U@users.noreply.github.com>
2023-10-12 16:01:56 -0700

    Set up funs and if's for coefficient and constant Need to build a way to switch between them still

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 
 // Variables
 
-let coeff;
-let constant;
+let coeff = 0;
+let constant = 0;
 let operator;
-let result;
+let result = 0;
 
 // let input;
 
 let currentDisplay = document.getElementsByClassName("display")[0];
 
+let outPutDisplay = currentDisplay.innerHTML
+
 // currentDisplay.innerHTML = input
+let isCoeff = true;
+let isConstant = false;
+
+
+// Decide to show coefficient or constant on display
+if (isCoeff == true) {
+  coeff = result
+  currentDisplay.innerHTML = coeff
+} else {
+  currentDisplay.innerHTML = constant
+}
 
 
 
-console.log(currentDisplay)
-// console.log(input)
+// Display in console
+console.log("Display" + outPutDisplay)
+
+
 
 // Add clicks to numbers
 let numButton = document.querySelectorAll(".num");
@@ -23,31 +38,49 @@ numButton.forEach((num) => {
   num.addEventListener("click", addInput)
 });
 
-input = numButton.innerHTML
+let input = numButton.innerHTML
 
 // Add numbers to display
 function addInput(event) {
-  let input = currentDisplay.innerHTML
-  let clickedNum = event.target
-  let temp = clickedNum.innerHTML
-  console.log(temp)
-  console.log(input)
-  if (currentDisplay.innerHTML.charAt(0) == 0) {
-    input = ""
-    currentDisplay.innerHTML = input + temp
+  let input = outPutDisplay;
+  let clickedNum = event.target;
+  let temp = clickedNum.innerHTML;
+
+  if (outPutDisplay.charAt(0) === '0') {
+    input = "";
+    outPutDisplay = input + temp;
   } else {
-    currentDisplay.innerHTML = input + temp
+    outPutDisplay = input + temp;
   }
-  return input
+  result = outPutDisplay;
+  console.log("result: " + result);
+
+  if (isCoeff) {
+    coeff = result;
+    currentDisplay.innerHTML = coeff; // Update the innerHTML here
+    console.log("cof " + coeff);
+  } else {
+    constant = result;
+    currentDisplay.innerHTML = constant;
+    console.log("con " + constant);
+  }
+
+  return result;
 }
+
+
 
 // Add click and clear func
 document.querySelector(".clear").addEventListener("click", clear)
 
 function clear(){
+  outPutDisplay = "0"
   currentDisplay.innerHTML = "0"
+  result = 0
   coeff = 0
   constant = 0
+  isCoeff = true;
+  isConstant = false;
 }
 
 
