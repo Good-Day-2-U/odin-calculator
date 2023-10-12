e391a10431a351693916518a0fc1b299b31eb9cb
Brandon <143155030+Good-Day-2-U@users.noreply.github.com>
2023-10-12 09:20:01 -0700

    Create layout of calc buttons

---
+++ script.js
@@ -0,0 +1,33 @@
+
+// Variables
+
+let coeff;
+let constant;
+let operator;
+let result;
+
+
+
+function equal() {
+
+}
+
+function percentage() {
+  
+}
+
+function add() {
+  
+}
+
+function subtract() {
+  
+}
+
+function multiply() {
+  
+}
+
+function divide() {
+  
+}
